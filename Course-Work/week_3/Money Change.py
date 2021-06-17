b58ba350b9c7b4337c078764062463e53a0c5799
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2021-06-17 10:05:17 +0530

    Create Money Change.py

---
+++ Course-Work/week_3/Money Change.py
@@ -0,0 +1,14 @@
+>>
+n = int(input())
+count = 0
+for i in [10, 5, 1]:
+    if n>=i:
+        q = n//i
+        count += q
+        n = n%i
+        if n==0:
+            print(count)
+            
+
+            
+@ CODED BY TSG405, 2021
