c53f0d91ac207fe29dd2018509415299bd0a5d64
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2021-06-17 10:55:47 +0530

    Create Maximum Salary (Largest Number).py

---
+++ Course-Work/week_3/Maximum Salary (Largest Number).py
@@ -0,0 +1,25 @@
+>>
+n = int(input())
+t = [int(i) for i in input().split()]
+
+def IsGreaterOrEqual(digit, max_digit):
+    return int(str(digit)+str(max_digit))>=int(str(max_digit)+str(digit))
+
+def largestnumber(lst):
+    answer = []
+    
+    while lst!=[]:
+        max_digit = 0
+        for digit in lst:
+            if IsGreaterOrEqual(digit, max_digit):
+                max_digit = digit
+        answer.append(max_digit)
+        lst.remove(max_digit)
+
+    return answer
+
+print(''.join([str(i) for i in largestnumber(t)]))
+
+
+
+@ CODED BY TSG405, 2021
